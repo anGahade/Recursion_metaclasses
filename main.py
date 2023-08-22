adcd6829ca96a0fe35e2d6454cc18efcf15a8aa4
Gahade <angrygahade@gmail.com>
2023-08-22 07:54:27 +0200

    Add func sum_of_digits

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+"""
+Напишіть рекурсивну функцію, яка обчислює суму цифр заданого числа.
 
+"""
+
+
+def sum_of_digits(n):
+    if n == 0:
+        return 0
+    else:
+        return n % 10 + sum_of_digits(n // 10)
+
+
+number = int(input("Введіть число: "))
+result = sum_of_digits(number)
+print(f"Сума цифр числа {number} дорівнює {result}")
